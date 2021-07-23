0844bab9a8514fe0a818b3637b32d9f6ba693134
Francisco Sanhueza <francisco.sanhueza.cid@gmail.com>
2021-07-23 07:00:32 -0400

    correcion de error

--- Fibonacci.php
+++ Fibonacci.php
@@ -1,10 +1,14 @@
 <?php
 $limite = $_POST['limite'];
-    $fibonacci  = [0,1];
-    for($i=2;$i<=$limite;$i++)
-    {
-        $fibonacci[] = $fibonacci[$i-1]+$fibonacci[$i-2];
-    }
+    if($limite!=null){
+        $fibonacci  = [0,1];
+        for($i=2;$i<=$limite;$i++)
+        {
+            $fibonacci[] = $fibonacci[$i-1]+$fibonacci[$i-2];
+        }
 
-echo join(' - ',$fibonacci);
+        echo join(' - ',$fibonacci);
+    }else{
+        echo 'no se ingreso un limite';
+    }
 ?>
